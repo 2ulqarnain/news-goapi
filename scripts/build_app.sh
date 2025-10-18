e67632fa8d8d35c3aad178259bd6f308701334db
2ulqarnain <zulqernaynh@gmail.com>
2025-10-19 00:08:20 +0500

    added build config for goland

---
+++ scripts/build_app.sh
@@ -0,0 +1,10 @@
+# build.sh
+#!/bin/bash
+echo -e "\033[1;34mBuilding App...\033[0m"
+go build -o app cmd/server/main.go
+
+if [ $? -eq 0 ]; then
+  echo -e "\033[1;32mBuild successful! -> ./app\033[0m"
+else
+  echo -e "\033[1;31m❌Build failed.\033[0m"
+fi
